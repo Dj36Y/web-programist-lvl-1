cf4a151841a124fdea56dcb6c06813e81964cc0e
Dj36Y <drozd.m4x@yandex.ru>
2020-08-09 14:54:14 +0300

    Work 3. 9 August

---
+++ Javascript/3 Js/script.js
@@ -0,0 +1,9 @@
+let step = 10
+
+function game(event){
+
+}
+p1.addEventListiner("click", game)
+p2.addEventListiner("click", game)
+console.log(event.taget)
+document.addEventListiner("keyup", game)
